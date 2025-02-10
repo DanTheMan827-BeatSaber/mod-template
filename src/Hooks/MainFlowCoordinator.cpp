cbd4164f571aa3809dd3eb7043d81878481cd01a
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-02-10 12:17:47 -0600

    Enhance code formatting and add an example hook for MainFlowCoordinator::DidActivate

---
+++ src/Hooks/MainFlowCoordinator.cpp
@@ -0,0 +1,32 @@
+// This file demonstrates how to hook the DidActivate method
+// of the MainFlowCoordinator class.
+
+#include "GlobalNamespace/MainFlowCoordinator.hpp"
+
+#include "Hooking.hpp"
+#include "Logger.hpp"
+
+using namespace GlobalNamespace;
+
+/// This hook does nothing but log a debug message.
+/// It serves as a basic example of how to hook a method.
+///
+/// The original method is called after this hook with no changes.
+MAKE_LATE_HOOK_MATCH(
+    MainFlowCoordinator_DidActivate,
+    &MainFlowCoordinator::DidActivate,
+    void,
+    MainFlowCoordinator* self,
+    bool firstActivation,
+    bool addedToHierarchy,
+    bool screenSystemEnabling
+) {
+    Logger.debug(
+        "MainFlowCoordinator::DidActivate called with firstActivation={}, addedToHierarchy={}, screenSystemEnabling={}",
+        firstActivation,
+        addedToHierarchy,
+        screenSystemEnabling
+    );
+
+    MainFlowCoordinator_DidActivate(self, firstActivation, addedToHierarchy, screenSystemEnabling);
+}
